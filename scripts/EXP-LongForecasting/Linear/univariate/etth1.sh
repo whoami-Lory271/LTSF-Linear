616293d867a3c29dc3e5ab8950f38c04e03ee9b8
Lorenzo De Santis <desantis.1849114@studenti.uniroma1.it>
2023-09-03 10:42:51 +0200

    fix some scripts

--- scripts/EXP-LongForecasting/Linear/univariate/etth1.sh
+++ scripts/EXP-LongForecasting/Linear/univariate/etth1.sh
@@ -9,9 +9,11 @@ fi
 if [ ! -d "./logs/LongForecasting/univariate" ]; then
     mkdir ./logs/LongForecasting/univariate
 fi
-model_name=DLinear
+model_name=NLinear
 
 # ETTh1, univariate results, pred_len= 24 48 96 192 336 720
+for pred_len in 24 48 168 336 720
+do
 python -u run_longExp.py \
   --is_training 1 \
   --root_path ./dataset/ \
@@ -20,74 +22,9 @@ python -u run_longExp.py \
   --model $model_name \
   --data ETTh1 \
   --seq_len 336 \
-  --pred_len 24 \
+  --pred_len $pred_len \
   --enc_in 1 \
   --des 'Exp' \
-  --itr 1 --batch_size 32 --feature S --learning_rate 0.005 >logs/LongForecasting/$model_name'_'fS_ETTh1_336_24.log
- 
-python -u run_longExp.py \
-  --is_training 1 \
-  --root_path ./dataset/ \
-  --data_path ETTh1.csv \
-  --model_id ETTh1_336_48 \
-  --model $model_name \
-  --data ETTh1 \
-  --seq_len 336 \
-  --pred_len 48 \
-  --enc_in 1 \
-  --des 'Exp' \
-  --itr 1 --batch_size 32 --feature S --learning_rate 0.005 >logs/LongForecasting/$model_name'_'fS_ETTh1_336_48.log
-
-python -u run_longExp.py \
-  --is_training 1 \
-  --root_path ./dataset/ \
-  --data_path ETTh1.csv \
-  --model_id ETTh1_336_96 \
-  --model $model_name \
-  --data ETTh1 \
-  --seq_len 336 \
-  --pred_len 96 \
-  --enc_in 1 \
-  --des 'Exp' \
-  --itr 1 --batch_size 32 --feature S --learning_rate 0.005 >logs/LongForecasting/$model_name'_'fS_ETTh1_336_96.log
-
-python -u run_longExp.py \
-  --is_training 1 \
-  --root_path ./dataset/ \
-  --data_path ETTh1.csv \
-  --model_id ETTh1_336_192 \
-  --model $model_name \
-  --data ETTh1 \
-  --seq_len 336 \
-  --pred_len 192 \
-  --enc_in 1 \
-  --des 'Exp' \
-  --itr 1 --batch_size 32 --feature S --learning_rate 0.005 >logs/LongForecasting/$model_name'_'fS_ETTh1_336_192.log
-
-python -u run_longExp.py \
-  --is_training 1 \
-  --root_path ./dataset/ \
-  --data_path ETTh1.csv \
-  --model_id ETTh1_336_336 \
-  --model $model_name \
-  --data ETTh1 \
-  --seq_len 336 \
-  --pred_len 336 \
-  --enc_in 1 \
-  --des 'Exp' \
-  --itr 1 --batch_size 32 --feature S --learning_rate 0.005 >logs/LongForecasting/$model_name'_'fS_ETTh1_336_336.log
-
-
-python -u run_longExp.py \
-  --is_training 1 \
-  --root_path ./dataset/ \
-  --data_path ETTh1.csv \
-  --model_id ETTh1_336_720 \
-  --model $model_name \
-  --data ETTh1 \
-  --seq_len 336 \
-  --pred_len 720 \
-  --enc_in 1 \
-  --des 'Exp' \
-  --itr 1 --batch_size 32 --feature S --learning_rate 0.005 >logs/LongForecasting/$model_name'_'fS_ETTh1_336_720.log
+  --itr 1 --batch_size 32 --feature S --learning_rate 0.005 >logs/LongForecasting/$model_name'_'fS_ETTh1_336_$pred_len.log
+done
 
--- scripts/EXP-LongForecasting/Linear/univariate/etth2.sh
+++ scripts/EXP-LongForecasting/Linear/univariate/etth2.sh
@@ -9,9 +9,11 @@ fi
 if [ ! -d "./logs/LongForecasting/univariate" ]; then
     mkdir ./logs/LongForecasting/univariate
 fi
-model_name=DLinear
+model_name=NLinear
 
 # ETTh2, univariate results, pred_len= 24 48 96 192 336 720
+for pred_len in 24 48 168 336 720
+do
 python -u run_longExp.py \
   --is_training 1 \
   --root_path ./dataset/ \
@@ -20,75 +22,8 @@ python -u run_longExp.py \
   --model $model_name \
   --data ETTh2 \
   --seq_len 336 \
-  --pred_len 24 \
+  --pred_len $pred_len \
   --enc_in 1 \
   --des 'Exp' \
-  --itr 1 --batch_size 32 --learning_rate 0.005 --feature S >logs/LongForecasting/$model_name'_'fS_ETTh2_336_24.log
-
-python -u run_longExp.py \
-  --is_training 1 \
-  --root_path ./dataset/ \
-  --data_path ETTh2.csv \
-  --model_id ETTh2_336_48 \
-  --model $model_name \
-  --data ETTh2 \
-  --seq_len 336 \
-  --pred_len 48 \
-  --enc_in 1 \
-  --des 'Exp' \
-  --itr 1 --batch_size 32 --learning_rate 0.005 --feature S >logs/LongForecasting/$model_name'_'fS_ETTh2_336_48.log
-
-python -u run_longExp.py \
-  --is_training 1 \
-  --root_path ./dataset/ \
-  --data_path ETTh2.csv \
-  --model_id ETTh2_336_96 \
-  --model $model_name \
-  --data ETTh2 \
-  --seq_len 336 \
-  --pred_len 96 \
-  --enc_in 1 \
-  --des 'Exp' \
-  --itr 1 --batch_size 32 --learning_rate 0.005 --feature S >logs/LongForecasting/$model_name'_'fS_ETTh2_336_96.log
-
-
-python -u run_longExp.py \
-  --is_training 1 \
-  --root_path ./dataset/ \
-  --data_path ETTh2.csv \
-  --model_id ETTh2_336_192 \
-  --model $model_name \
-  --data ETTh2 \
-  --seq_len 336 \
-  --pred_len 192 \
-  --enc_in 1 \
-  --des 'Exp' \
-  --itr 1 --batch_size 32 --learning_rate 0.005 --feature S >logs/LongForecasting/$model_name'_'fS_ETTh2_336_192.log
-
-
-python -u run_longExp.py \
-  --is_training 1 \
-  --root_path ./dataset/ \
-  --data_path ETTh2.csv \
-  --model_id ETTh2_336_336 \
-  --model $model_name \
-  --data ETTh2 \
-  --seq_len 336 \
-  --pred_len 336 \
-  --enc_in 1 \
-  --des 'Exp' \
-  --itr 1 --batch_size 32 --learning_rate 0.005 --feature S >logs/LongForecasting/$model_name'_'fS_ETTh2_336_336.log
- 
-
-python -u run_longExp.py \
-  --is_training 1 \
-  --root_path ./dataset/ \
-  --data_path ETTh2.csv \
-  --model_id ETTh2_336_720 \
-  --model $model_name \
-  --data ETTh2 \
-  --seq_len 336 \
-  --pred_len 720 \
-  --enc_in 1 \
-  --des 'Exp' \
-  --itr 1 --batch_size 32 --learning_rate 0.005 --feature S >logs/LongForecasting/$model_name'_'fS_ETTh2_336_720.log
+  --itr 1 --batch_size 32 --learning_rate 0.005 --feature S >logs/LongForecasting/$model_name'_'fS_ETTh2_336_$pred_len.log
+done
