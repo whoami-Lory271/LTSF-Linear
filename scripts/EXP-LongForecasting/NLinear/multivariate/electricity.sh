143f7f261c6361fc2ba5a9164fd62875b1950c06
Lorenzo De Santis <desantis.1849114@studenti.uniroma1.it>
2023-09-16 16:54:40 +0200

    fix some scripts

--- scripts/EXP-LongForecasting/NLinear/multivariate/electricity.sh
+++ scripts/EXP-LongForecasting/NLinear/multivariate/electricity.sh
@@ -13,7 +13,7 @@ model_name=NLinear
 seq_len=336
 
 # ETTm1, univariate results, pred_len= 96 192 336 720
-for pred_len in 336 720 #24 48 168 336 720
+for pred_len in 168 #24 48 168 336 720
 do
 python -u run_longExp.py \
   --is_training 1 \
@@ -27,5 +27,6 @@ python -u run_longExp.py \
   --pred_len $pred_len \
   --enc_in 321 \
   --des 'Exp' \
+  --seed 1
   --itr 1 --batch_size 16  --learning_rate 0.001 --individual
 done
