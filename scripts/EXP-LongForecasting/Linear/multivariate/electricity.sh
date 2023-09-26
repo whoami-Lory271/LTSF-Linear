2998e0913fe843fe86e8fb948813be82eb30bda5
Lorenzo De Santis <desantis.1849114@studenti.uniroma1.it>
2023-09-26 09:28:12 +0200

    fix some scripts

--- scripts/EXP-LongForecasting/Linear/multivariate/electricity.sh
+++ scripts/EXP-LongForecasting/Linear/multivariate/electricity.sh
@@ -13,19 +13,23 @@ model_name=Linear
 seq_len=336
 
 # ETTm1, univariate results, pred_len= 96 192 336 720
-for pred_len in 336 720 #24 48 168 336 720
+for seed in 1 2 3
 do
-python -u run_longExp.py \
-  --is_training 1 \
-  --root_path ./dataset/ \
-  --data_path electricity.csv \
-  --model_id Electricity_$seq_len'_'$pred_len \
-  --model $model_name \
-  --data custom \
-  --features M \
-  --seq_len $seq_len \
-  --pred_len $pred_len \
-  --enc_in 321 \
-  --des 'Exp' \
-  --itr 1 --batch_size 16  --learning_rate 0.001 --individual
+  for pred_len in 168 336 720 #24 48 168 336 720
+  do
+  python -u run_longExp.py \
+    --is_training 1 \
+    --root_path ./dataset/ \
+    --data_path electricity.csv \
+    --model_id Electricity_$seq_len'_'$pred_len \
+    --model $model_name \
+    --data custom \
+    --features M \
+    --seq_len $seq_len \
+    --pred_len $pred_len \
+    --enc_in 321 \
+    --des 'Exp' \
+    --seed $seed \
+    --itr 1 --batch_size 16  --learning_rate 0.001 --individual
+  done
 done
